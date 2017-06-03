bf24f20d0cc426d2131fec336705a62344ecfdd7
LNATION <thisusedtobeanemail@gmail.com>
2017-06-03 16:04:28 +0100

    fix returns update Makefile as Type::Tiny is now at a stable version

--- t/odea/Returns.pm
+++ t/odea/Returns.pm
@@ -6,14 +6,15 @@ use Types::Standard qw/Str ArrayRef HashRef/;
 
 validate_subs (
     [qw/hello_hash hello_hashref/] => {
-        params => {
+        returns => {
             one   => [ Str, sub { 'I Have a Default Value' } ],
             two   => [ ArrayRef ],
             three => [ HashRef ],
-        },
+        	four  => [ Str ],
+		},
     },
     [qw/a_list a_single_arrayref/] => {
-        params => [ [Str], [ArrayRef], [HashRef] ],
+        returns => [ [Str], [ArrayRef], [HashRef], [Str] ],
     },
 );
 
@@ -33,18 +34,20 @@ sub hello_hashref {
 
 sub a_list {
     my ($self, @args) = @_;    
-    return @args;
+ 	push @args, 'd';
+	return @args;
 }
 
 sub a_single_arrayref {
     my ($self, $args) = @_;    
-    return $args;
+	push @{ $args }, 'd';
+	return $args;
 }
 
 sub okay_test {
     my ($self) = shift;
-    my ($one, $two, $three) = $self->a_list( 'a', ['b'], { four => 'ahh' } );
-    return ($one, $two, $three);
+    my ($one, $two, $three, $four) = $self->a_list( 'a', ['b'], { four => 'ahh' } );
+    return ($one, $two, $three, $four);
 }
 
 1;
